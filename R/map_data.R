f2dbf57996882e969edd2024696665dfc8f35138
liz-fw <lstebbins@flowwest.com>
2023-01-09 11:45:37 -0800

    adds documentation, vignette, and updated mapping function

--- R/map_data.R
+++ R/map_data.R
@@ -6,7 +6,9 @@ library(leaflet)
 
 #' @title Map Urban Water Data
 #' @description This function generates a county-level map of Urban Water Data.
-#' @param data Data to plot. Must be in the format outputted from \code{pull_data()} function
+#' @param data Data to plot. Must be in the format outputted from \code{pull_data()} function and to avoid
+#' summing duplicate rows of data in a county, please only use data with \code{category} "demand_total"
+#' or "supply_total".
 #' @examples
 #' map_data(data)
 #' @export
@@ -23,18 +25,29 @@ map_county_data <- function(data) {
 
   # join counties to data
   county_level_data <- left_join(data, pwsid_data, by = "pwsid") |>
-    group_by(county) |>
+    group_by(county, year) |>
     summarise(total_county_af = sum(volume_af, na.rm = T),
               no_urban_water_suppliers = n_distinct(supplier_name)) |>
     distinct(county, total_county_af, no_urban_water_suppliers)
 
   # create palette and labels
   pal_county_af <- colorNumeric(palette = "BuPu", log(county_level_data$total_county_af))
-  label_suppliers <- function(county, no_suppliers, total_county_af) {
-    paste(str_glue("No. of urban water suppliers in {stringr::str_to_title(county)} county: {no_suppliers}"),
-          "<br>",
-          str_glue("Demand in acre feet: {total_county_af}"))
+
+  if(stringr::str_detect(unique(data$category), "demand")) {
+    label_suppliers <- function(county, no_suppliers, total_county_af) {
+      paste(str_glue("No. of urban water suppliers in {stringr::str_to_title(county)} county: {no_suppliers}"),
+            "<br>",
+            str_glue("Demand in acre feet: {round(total_county_af,0)}"))
+    }
   }
+  if(stringr::str_detect(unique(data$category), "supply")) {
+    label_suppliers <- function(county, no_suppliers, total_county_af) {
+      paste(str_glue("No. of urban water suppliers in {stringr::str_to_title(county)} county: {no_suppliers}"),
+            "<br>",
+            str_glue("Supply in acre feet: {round(total_county_af,0)}"))
+    }
+  }
+
 
   # get shapefiles and filter to matching counties in CA
   USA <- raster::getData("GADM", country = "usa", level = 2)
@@ -44,7 +57,7 @@ map_county_data <- function(data) {
 
   # leaflet plot
   leaflet() |> addTiles() |>
-    setView(lat = 36.778259, lng = -119.417931, zoom = 6) |>
+    setView(lat = 36.778259, lng = -119.417931, zoom = 5) |>
     addPolygons(data = USA,
                 fillColor = ~pal_county_af(log(county_level_data$total_county_af)),
                 fillOpacity = 0.8,
